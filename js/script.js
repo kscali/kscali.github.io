a3b32df6a53fdcbb0061b2211981a5472814b234
koy <kosatun@aol.com>
2019-06-14 12:29:28 -0700

    updated skills and intro

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(window).on("load", function() {
+  var type = new Typed(".typed", {
+    strings: [
+      "Software Engineer.",
+      "Web Developer.",
+      "Music Lover.",
+      "Lifelong Learner.",
+      "Full Stack Developer"
+    ],
+    typeSpeed: 70,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false
+  });
+});
